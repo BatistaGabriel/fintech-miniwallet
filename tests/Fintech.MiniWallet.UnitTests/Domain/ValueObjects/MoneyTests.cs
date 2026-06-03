655ed196174ddf12336438c1767dcb525fc46d0f
Gahbree0x4c <aller.vous@pm.me>
2026-06-03 14:34:49 -0300

    refactor: add minor refactors to the `Money` value object

--- tests/Fintech.MiniWallet.UnitTests/Domain/ValueObjects/MoneyTests.cs
+++ tests/Fintech.MiniWallet.UnitTests/Domain/ValueObjects/MoneyTests.cs
@@ -8,15 +8,15 @@ public class MoneyTests
     [Fact]
     public void Should_Throw_If_Money_Is_Negative()
     {
-        Action act = () => new Money(-10);
+        Action act = () => new Money(-10m);
         act.Should().Throw<ArgumentException>();
     }
 
     [Fact]
     public void Should_Be_Equal_When_Same_Value()
     {
-        Money primaryMoney = new Money(10);
-        Money secondaryMoney = new Money(10);
+        Money primaryMoney = new Money(10m);
+        Money secondaryMoney = new Money(10m);
 
         primaryMoney.Should().Be(secondaryMoney);
     }
@@ -24,10 +24,10 @@ public class MoneyTests
     [Fact]
     public void Should_Return_The_Sum_Of_Two_Money_Values()
     {
-        Money primaryMoney = new Money(10);
-        Money secondaryMoney = new Money(20);
+        Money primaryMoney = new Money(10m);
+        Money secondaryMoney = new Money(20m);
 
         Money result = primaryMoney + secondaryMoney;
-        result.Should().Be(new Money(30));
+        result.Should().Be(new Money(30m));
     }
 }
